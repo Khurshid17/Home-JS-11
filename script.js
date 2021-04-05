3fb4b7d9c14428edc50faca25124a81fe645b312
Khurshid <rakhmatullaevkhurshid17@gmail.com>
2021-04-05 14:17:56 +0300

    scroll

---
+++ script.js
@@ -0,0 +1,47 @@
+class Scroll {
+    constructor(obj){
+        if (typeof obj.el == "string") {
+            this.element = document.querySelector(obj.el);
+        } else if (obj.el instanceof HTMLElement) { // instanceof - проверяет наличие в наследниках класса HTMLElement
+            this.element = obj.el;
+        }
+        this.element.style.position = "fixed";
+        this.top = obj.top;
+        this.unit = obj.unit;
+        this.scroll();
+        window.addEventListener("scroll", ()=>{this.scroll()});
+        window.addEventListener("resize", ()=>{this.scroll()});
+    }
+    scroll() {
+        this.menuTop = this.scrollNumber()
+        console.log(window.pageYOffset);
+        if (this.menuTop - window.pageYOffset > 0) {
+            this.element.style.top = this.menuTop - window.pageYOffset + "px";            
+        } else {
+            this.element.style.top = 0;
+        }
+    }
+    scrollNumber() {
+        if (this.unit == "px") {
+            return this.top >= 0 ? this.top : 0;
+        } else if (this.unit == "%" || this.unit == undefined) {
+            return window.innerHeight / 100 * this.top - this.element.clientHeight;
+        }
+    }
+}
+
+let y = document.querySelector('.header__nav');
+const x = new Scroll({
+    // el: ".header__nav"
+    el: y,
+    top: 100,
+    unit: "%"
+});
+console.dir(x.element);
+
+let div = document.querySelector('.header__content');
+
+div.addEventListener("mouseover", function () {
+    div.style.top = `${Math.random() * 80}%`
+    div.style.left = `${Math.random() * 80}%`
+})
